872bc0e70cc8d4f21bc725bb221de2a48649695d
Ashley Marlow <ashley.marlow@hotmail.com>
2023-02-11 20:16:49 -0800

    more on nav and icons

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function setupPalettes(data) {
     let palettesArray = data.palettes;
     let palettehtml = '';
     for (let palette of palettesArray) {
-        let name = palette.name;
+        // let name = palette.name;
         // let tag = palette.tag;
         let color1 = palette.color1;
         let color2 = palette.color2;
@@ -22,21 +22,18 @@ function setupPalettes(data) {
         let color4 = palette.color4;
         let color5 = palette.color5;
         let color6 = palette.color6;
-        console.log(color1);
         palettehtml += `
             <div class="palette">
                 <div class="row header-row">
-                    <div class="col">
-                        <h2>` + name + `</h2>
-                    </div>
+                   
                 </div>
                 <div class="row palette-row">
-                    <div class="palette-color" style="background-color:` + color1 + `";><span>` + color1 + `</span></div>
-                    <div class="palette-color" style="background-color:` + color2 + `";><span>` + color2 + `</span></div>
-                    <div class="palette-color" style="background-color:` + color3 + `";><span>` + color3 + `</span></div>
-                    <div class="palette-color" style="background-color:` + color4 + `";><span>` + color4 + `</span></div>
-                    <div class="palette-color" style="background-color:` + color5 + `";><span>` + color5 + `</span></div>
-                    <div class="palette-color" style="background-color:` + color6 + `";><span>` + color6 + `</span></div>
+                    <div class="palette-color" style="background-color:` + color1 + `";><span>` + color1 + `<img src="img/copy.svg"></span></div>
+                    <div class="palette-color" style="background-color:` + color2 + `";><span>` + color2 + `<img src="img/copy.svg"></span></div>
+                    <div class="palette-color" style="background-color:` + color3 + `";><span>` + color3 + `<img src="img/copy.svg"></span></div>
+                    <div class="palette-color" style="background-color:` + color4 + `";><span>` + color4 + `<img src="img/copy.svg"></span></div>
+                    <div class="palette-color" style="background-color:` + color5 + `";><span>` + color5 + `<img src="img/copy.svg"></span></div>
+                    <div class="palette-color" style="background-color:` + color6 + `";><span>` + color6 + `<img src="img/copy.svg"></span></div>
                 </div>
             </div>
         `;
